e343c0d65e009e16d46c0a17f943367f4ac3ca46
Khatchi <solomon9ice2014@gmail.com>
2023-08-02 04:28:14 -0700

    ./100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-log_content = File.read(ARGV[0])
-matches = log_content.scan(/(\S+),(\S+),(\S+)/)
-puts matches.map { |sender, receiver, flags| "#{sender},#{receiver},#{flags}" }.join("\n")
+matches = ARGV[0].scan(/(?<=\[from:).*?(?=\])|(?<=\[to:).*?(?=\])|(?<=\[flags:).*?(?=\])/)
+puts matches.join(",")
 
