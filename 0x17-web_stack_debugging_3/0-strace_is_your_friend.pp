24fdb7bf8b3a1c192e7c8ba02abf5cf2fda9604e
Khatchi <solomon9ice2014@gmail.com>
2023-11-08 16:48:15 +0100

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
+# Use the file_edit resource type to edit a specific file
 exec { 'fix_line':
   command     => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  provider => 'windows terminal'
 }
+
